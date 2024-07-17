2ab2c0d3759e88cedac11a80620aa4adc3a36943
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-07-17 15:19:24 +0300

    Completes Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        anagrams = []
+        sorted_word = sorted(self.word)
+        for i in word_list:
+            if sorted(i) == sorted_word:
+                anagrams.append(i)
+        return anagrams
+
+listen = Anagram("listen")
+
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
